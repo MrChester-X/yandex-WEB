df2464a6a11a1788fcef3c31b8536fe5d2bd36bb
MrChester-X <bulychev_platon1@mail.ru>
2022-02-25 17:21:42 +0300

    Добавляем капитана fix 2

--- WEB. Знакомство с flask-sqlalchemy/Добавляем капитана/main.py
+++ WEB. Знакомство с flask-sqlalchemy/Добавляем капитана/main.py
@@ -9,12 +9,18 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/blogs.db")
 
+    data = [
+        ["Scott", "Ridley", 21, "captain", "research engineer", "module_1", "scott_chief@mars.org"],
+        ["Butler", "Christopher", 19, "deputy captain", "research engineer", "module_2", "butler_deputy@mars.org"],
+        ["Edwards", "Charles", 24, "head guard", "security", "module_3", "edwards_security@mars.org"],
+        ["Sparks", "Joseph", 23, "chef", "cook", "module_4", "sparks_cook_great@mars.org"],
+        ["Cannon", "Logan", 20, "lead", "software engineer", "module_5", "cannon_back_coder@mars.org"]
+    ]
+
     db_sess = db_session.create_session()
-    add_user(db_sess, "Scott", "Ridley", 21, "captain", "research engineer", "module_1", "scott_chief@mars.org")
-    add_user(db_sess, "Butler", "Christopher", 19, "deputy captain", "research engineer", "module_2", "butler_deputy@mars.org")
-    add_user(db_sess, "Edwards", "Charles", 24, "head guard", "security", "module_3", "edwards_security@mars.org")
-    add_user(db_sess, "Sparks", "Joseph", 23, "chef", "cook", "module_4", "sparks_cook_great@mars.org")
-    add_user(db_sess, "Cannon", "Logan", 20, "lead", "software engineer", "module_5", "cannon_back_coder@mars.org")
+
+    for user in data:
+        add_user(db_sess, *user)
 
     # app.run()
 
